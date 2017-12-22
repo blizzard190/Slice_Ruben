1863e9719f71f5a105022f6f520c2978d9af6e5c
blizzard190 <22256@ma-web.nl>
2017-12-22 15:18:13 +0100

    My scripts
    My scripts

---
+++ Camera/Camera_Move.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Move : MonoBehaviour
+{
+    public GameObject camera1;
+    private bool trap = false;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && trap == false)
+        {
+            MoveCamera(new Vector3(-1.71f, 2.89f, -7.08f),new Vector3(25.2f, -90, 0));
+            trap = true;
+        }
+        else if (other.gameObject.tag == "Player" && trap == true)
+        {
+            MoveCamera(new Vector3(0.1965061f, 2.68f, -8.287f),new Vector3(32.27f, -90, 0));
+            trap = false;
+        }
+    }
+
+    private void MoveCamera(Vector3 position, Vector3 rotation)
+    {
+        iTween.MoveTo(camera1, position, 2f);
+        iTween.RotateTo(camera1, rotation, 2f);
+    }
+
+}
---
+++ Camera/Camera_change.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_change : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject CameraPoint;
+    public GameObject CameraPoint1;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && Camera.main.transform.position != CameraPoint.transform.position)
+        {
+            MoveCamera(CameraPoint.transform.position, new Vector3(17.74f, -90, 0));
+        }
+        else if (other.gameObject.tag == "Player" && Camera.main.transform.position != CameraPoint1.transform.position)
+        {
+            MoveCamera(CameraPoint1.transform.position, new Vector3(62.35f, -90, 0));
+        }
+    }
+
+    private void MoveCamera(Vector3 position, Vector3 rotation)
+    {
+        Camera.main.transform.position = position;
+        Camera.main.transform.rotation = Quaternion.Euler(rotation);
+    }
+}
